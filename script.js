6bd3d0113dedc89d8baf50a32e0a2a84f22fd01d
yhcho0405 <yhcho0405@users.noreply.github.com>
2019-12-23 21:54:54 +0900

    Revert "Revert "icon""
    This reverts commit 4f26d44b3e3a26e909d1ff21022317748c6f52aa.

---
+++ script.js
@@ -0,0 +1,27 @@
+function togglePlay() {
+  var v = document.querySelector('video');
+
+  if (v == null) return;
+
+  (v.paused) ? v.play() : v.pause();
+}
+chrome.tabs.executeScript({
+  code:'document.querySelector("body").innerText;'
+}, function(result){
+
+  var bodyText = result[0];
+
+  var bodySep = bodyText.split(' ');
+
+  var bodyTime = bodySep[1];
+
+  var textFin = bodyTime.split('\n');
+
+  var bodyTimeFin = textFin[2] + "s";
+
+  var timeParameter = bodyTimeFin.replace(':', 'm');
+
+  togglePlay();
+
+  document.querySelector('#user').innerText = timeParameter;
+});
