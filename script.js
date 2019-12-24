783847ce5bed0291d372c1a42a10a907911d08f2
yhcho0405 <yhcho0405@users.noreply.github.com>
2019-12-25 05:26:16 +0900

    링크 추출
    추출 후 시간 코드와 합침

--- script.js
+++ script.js
@@ -2,24 +2,31 @@
 chrome.tabs.executeScript({
   code:'jQuery("#img").attr("src");'
 }, function(sourceProfile){
-  alert(sourceProfile);
+  //alert(sourceProfile);
 });
+
 //time
 chrome.tabs.executeScript({
   code:'document.querySelector("body").innerText;'
 }, function(result){
-
   var bodyText = result[0];
-
   var bodySep = bodyText.split(' ');
-
   var bodyTime = bodySep[1];
-
   var textFin = bodyTime.split('\n');
-
   var bodyTimeFin = textFin[2] + "s";
-
   var timeParameter = bodyTimeFin.replace(':', 'm');
 
-  document.querySelector('#user').innerText = result[0];
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
+    var url = tabs[0].url.substring(32,);
+    var linkFin ="https://youtu.be/"+url+"?t="+timeParameter;
+    alert(linkFin);
+  });
 });
+
+//url
+
+//document.querySelector('#user').innerText = ;
+
+
+//var linkFin ="https://youtu.be/"+ +"?t="+timeParameter;
+//alert(linkFin);
