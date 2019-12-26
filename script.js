cee6b5f2b4d7a7bd40d91cb21d935707a8866d43
yhcho0405 <yhcho0405@users.noreply.github.com>
2019-12-26 20:43:42 +0900

    조금 수정
    ㅈㄱㄴ

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //profile
 chrome.tabs.executeScript({
-  code:'jQuery("#img").attr("src");'
-}, function(sourceProfile){
+  code: 'jQuery("#img").attr("src");'
+}, function(sourceProfile) {
   //alert(sourceProfile);
 });
 
 //time
 chrome.tabs.executeScript({
-  code:'document.querySelector("body").innerText;'
-}, function(result){
+  code: 'document.querySelector("body").innerText;'
+}, function(result) {
   var bodyText = result[0];
   var bodySep = bodyText.split(' ');
   var bodyTime = bodySep[1];
@@ -16,10 +16,14 @@ chrome.tabs.executeScript({
   var bodyTimeFin = textFin[2] + "s";
   var timeParameter = bodyTimeFin.replace(':', 'm');
 
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
-    var url = tabs[0].url.substring(32,);
-    var linkFin ="https://youtu.be/"+url+"?t="+timeParameter;
-    alert(linkFin);
+  chrome.tabs.query({
+    'active': true,
+    'lastFocusedWindow': true,
+    'currentWindow': true
+  }, function(tabs) {
+    var url = tabs[0].url.substring(32, 43);
+    var linkFin = "https://youtu.be/" + url + "?t=" + timeParameter;
+    document.querySelector('#user').innerText = linkFin;
   });
 });
 
