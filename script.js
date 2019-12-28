695abaa3e5a34cb0dfb32815d938ccbd4b4a56fb
yhcho0405 <yhcho0405@users.noreply.github.com>
2019-12-28 22:40:25 +0900

    버튼클릭이벤트
    수정필요

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 $(".div6").hide();
-if(inYoutube) {
-  if(inVideo) {
+if (inYoutube) {
+  if (inVideo) {
+    var bmkBtn = document.getElementById('btn1');
+    var delBtn = document.getElementById('btn2');
+    bmkBtn.addEventListener('click', function(event) {
+      var bmkHistory = document.getElementById('bmkHistory');
+      var resultDiv = document.createElement("div");
+      var resultText = '<input type="button" id="btn1" class="button" value="BOOKMARKING!">';
+      resultDiv.appendChild(document.createTextNode(resultText));
+      bmkHistory.insertBefore(resultDiv, bmkHistory.firstChild);
+    });
     //time
     chrome.tabs.executeScript({
       code: 'document.querySelector("body").innerText;'
@@ -13,13 +22,11 @@ if(inYoutube) {
       var bodyTimeFin2 = textFin[2] + "s";
       var timeParameter1 = bodyTimeFin1.replace(':', 'm');
       var timeParameter2 = bodyTimeFin2.replace(':', 'm');
-      if(timeParameter1.length == 5 && bodyTimeFin1.indexOf(":") != -1) {
+      if (timeParameter1.length == 5 && bodyTimeFin1.indexOf(":") != -1) {
         timeParameter = timeParameter1;
-      }
-      else if(timeParameter2.length == 5 && bodyTimeFin2.indexOf(":") != -1) {
+      } else if (timeParameter2.length == 5 && bodyTimeFin2.indexOf(":") != -1) {
         timeParameter = timeParameter2;
-      }
-      else {
+      } else {
         alert("error! code: 01 \n(Please send an email to yhcho0405@kakao.com)");
       }
       chrome.tabs.executeScript({
@@ -29,10 +36,9 @@ if(inYoutube) {
           code: 'document.querySelector("#notification-count").innerText;'
         }, function(notiCount) {
           strTitle = videoTitle.toString();
-          if(notiCount == 0) {
-            titleFin = strTitle.substring(0, strTitle.length - 10);//알림이 없을 때
-          }
-          else {
+          if (notiCount == 0) {
+            titleFin = strTitle.substring(0, strTitle.length - 10); //알림이 없을 때
+          } else {
             titleFin = strTitle.substring(strTitle.indexOf(")") + 2, strTitle.length - 10); //알림이 있을 때
           }
           //else
@@ -50,7 +56,7 @@ if(inYoutube) {
       }, function(tabs) {
         var url = tabs[0].url.substring(32, 43);
         var linkFin = "https://youtu.be/" + url + "?t=" + timeParameter;
-        document.querySelector('#user').innerText = linkFin;
+        document.querySelector('#link').innerText = linkFin;
       });
     });
   } else {
