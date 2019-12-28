bade503f6a494d7ed1d19c6030d1e77e956c99cc
yhcho0405 <yhcho0405@users.noreply.github.com>
2019-12-28 10:31:37 +0900

    예외처리 3차
    ㅈㄱㄴ

--- exception.js
+++ exception.js
@@ -1,11 +1,17 @@
 var inYoutube = true;
+var inVideo = true;
+
 chrome.tabs.query({
   'active': true,
   'lastFocusedWindow': true,
   'currentWindow': true
 }, function(tabs) {
+
   var url = tabs[0].url;
-  if(url.indexOf("youtube") == -1) {
+  if(url.indexOf("youtube.com") == -1) {
     inYoutube = false;
+  }
+  if(url.indexOf("watch") == -1) {
+    inVideo = false;
   }
 });
--- script.js
+++ script.js
@@ -1,62 +1,60 @@
+$(".div6").hide();
 if(inYoutube) {
-  //time
-  chrome.tabs.executeScript({
-    code: 'document.querySelector("body").innerText;'
-  }, function(result) {
-    var bodyText = result[0];
-    var bodySep = bodyText.split(' ');
-    var bodyTime = bodySep[1];
-    var textFin = bodyTime.split('\n');
-    var bodyTimeFin1 = textFin[1] + "s";
-    var bodyTimeFin2 = textFin[2] + "s";
-    var timeParameter1 = bodyTimeFin1.replace(':', 'm');
-    var timeParameter2 = bodyTimeFin2.replace(':', 'm');
-    if(timeParameter1.length == 5 && bodyTimeFin1.indexOf(":") != -1) {
-      timeParameter = timeParameter1;
-    }
-    else if(timeParameter2.length == 5 && bodyTimeFin2.indexOf(":") != -1) {
-      timeParameter = timeParameter2;
-    }
-    else {
-      alert("error! code: 01 \n(Please send an email to yhcho0405@kakao.com)");
-    }
+  if(inVideo) {
+    //time
     chrome.tabs.executeScript({
-      code: 'document.querySelector("head").querySelector("title").innerText;'
-    }, function(videoTitle) {
+      code: 'document.querySelector("body").innerText;'
+    }, function(result) {
+      var bodyText = result[0];
+      var bodySep = bodyText.split(' ');
+      var bodyTime = bodySep[1];
+      var textFin = bodyTime.split('\n');
+      var bodyTimeFin1 = textFin[1] + "s";
+      var bodyTimeFin2 = textFin[2] + "s";
+      var timeParameter1 = bodyTimeFin1.replace(':', 'm');
+      var timeParameter2 = bodyTimeFin2.replace(':', 'm');
+      if(timeParameter1.length == 5 && bodyTimeFin1.indexOf(":") != -1) {
+        timeParameter = timeParameter1;
+      }
+      else if(timeParameter2.length == 5 && bodyTimeFin2.indexOf(":") != -1) {
+        timeParameter = timeParameter2;
+      }
+      else {
+        alert("error! code: 01 \n(Please send an email to yhcho0405@kakao.com)");
+      }
       chrome.tabs.executeScript({
-        code: 'document.querySelector("#notification-count").innerText;'
-      }, function(notiCount) {
-        strTitle = videoTitle.toString();
-        if(notiCount == 0) {
-          titleFin = strTitle.substring(0, strTitle.length - 10);//알림이 없을 때
-        }
-        else if(notiCount <= 9){
-          titleFin = strTitle.substring(4, strTitle.length - 10); //알림이 한자리 수
-        }
-        else if(notiCount >= 10)
-        {
-          titleFin = strTitle.substring(5, strTitle.length - 10); //알림이 두자리 수
-        }
-        document.querySelector('#title').innerText = titleFin;
+        code: 'document.querySelector("head").querySelector("title").innerText;'
+      }, function(videoTitle) {
+        chrome.tabs.executeScript({
+          code: 'document.querySelector("#notification-count").innerText;'
+        }, function(notiCount) {
+          strTitle = videoTitle.toString();
+          if(notiCount == 0) {
+            titleFin = strTitle.substring(0, strTitle.length - 10);//알림이 없을 때
+          }
+          else {
+            titleFin = strTitle.substring(strTitle.indexOf(")") + 2, strTitle.length - 10); //알림이 있을 때
+          }
+          //else
+          //{
+          //  titleFin = strTitle.substring(5, strTitle.length - 10); //알림이 두자리 수
+          //}
+          document.querySelector('#title').innerText = titleFin;
+        });
+      });
+
+      chrome.tabs.query({
+        'active': true,
+        'lastFocusedWindow': true,
+        'currentWindow': true
+      }, function(tabs) {
+        var url = tabs[0].url.substring(32, 43);
+        var linkFin = "https://youtu.be/" + url + "?t=" + timeParameter;
+        document.querySelector('#user').innerText = linkFin;
       });
     });
-
-    chrome.tabs.query({
-      'active': true,
-      'lastFocusedWindow': true,
-      'currentWindow': true
-    }, function(tabs) {
-      var url = tabs[0].url.substring(32, 43);
-      var linkFin = "https://youtu.be/" + url + "?t=" + timeParameter;
-      document.querySelector('#user').innerText = linkFin;
-    });
-  });
+  } else {
+    $("#btn1").hide();
+    $(".div6").show();
+  }
 }
-
-
-
-//chrome.tabs.executeScript({
-//  code: '$(document).find("yt-formatted-string").text();'
-//}, function(videoTitle) {
-//  alert(videoTitle);
-//});
